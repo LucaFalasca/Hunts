62bc99bef403c864db1d47f459084922fd19a0cb
andreamancuso098 <56720907+andreamancuso098@users.noreply.github.com>
2021-04-21 16:49:26 +0200

    #20

---
+++ src/logic/model/entity/Object.java
@@ -0,0 +1,33 @@
+package logic.model.entity;
+
+public class Object {
+private int idObject;
+	
+	private String name;
+	
+	private String path;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public int getIdObject() {
+		return idObject;
+	}
+
+	public void setIdObject(int idObject) {
+		this.idObject = idObject;
+	}
+}
---
+++ src/logic/model/entity/Riddle.java
@@ -0,0 +1,64 @@
+package logic.model.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Riddle {
+private int nRiddle;
+	
+	private String riddleText;
+	
+	private String solutionText;
+	
+	private List<String> clueList = new ArrayList<>();
+	
+	private String reward;
+	
+	public int getnRiddle() {
+		return nRiddle;
+	}
+
+	public void setnRiddle(int nRiddle) {
+		this.nRiddle = nRiddle;
+	}
+
+	public String getRiddleText() {
+		return riddleText;
+	}
+
+	public void setRiddleText(String riddleText) {
+		this.riddleText = riddleText;
+	}
+
+	public String getSolutionText() {
+		return solutionText;
+	}
+
+	public void setSolutionText(String solutionText) {
+		this.solutionText = solutionText;
+	}
+
+	public List<String> getClueList() {
+		return clueList;
+	}
+	
+	public void setClueList(List<String> clueList) {
+		this.clueList = clueList;
+	}
+	
+	public void setClueListElement(int index, String clue) {
+		clueList.add(index, clue);
+	}
+	
+	public String getClueListElement(int index) {
+		return clueList.get(index);
+	}
+
+	public String getReward() {
+		return reward;
+	}
+
+	public void setReward(String reward) {
+		this.reward = reward;
+	}
+}
