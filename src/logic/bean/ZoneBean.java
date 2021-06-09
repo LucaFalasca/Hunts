3b1e000932813bb36f63b4a94537fa33fdf685ef
LucaFalasca <30274870+LucaFalasca@users.noreply.github.com>
2021-06-09 19:38:19 +0200

    #5 - Refactoring Code Smell

--- src/logic/bean/ZoneBean.java
+++ src/logic/bean/ZoneBean.java
@@ -12,7 +12,7 @@ public class ZoneBean {
 	public ZoneBean(String nameZone, double x1, double y1, double x2, double y2, String shape) {
 		this.nameZone = nameZone;
 		this.x1 = x1;
-		this.y1 = x2;
+		this.y1 = y1;
 		this.x2 = x2;
 		this.y2 = y2;
 		this.shape = shape;
