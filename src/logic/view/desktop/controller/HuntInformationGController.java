8a3ec1f80c1476a64c4d8dd636d7513438b92229
Andrea Mancuso <56720907+andreamancuso098@users.noreply.github.com>
2021-07-11 15:33:46 +0200

    #223 - Logic for review's item in more information

--- src/logic/view/desktop/controller/HuntInformationGController.java
+++ src/logic/view/desktop/controller/HuntInformationGController.java
@@ -1,20 +1,29 @@
 package logic.view.desktop.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import logic.bean.HuntBean;
+import logic.bean.MapBean;
+import logic.bean.PlayedHuntBean;
+import logic.bean.ReviewBean;
 import logic.control.ManageHuntControl;
+import logic.control.PlayHuntControl;
 import logic.enumeration.Pages;
 import logic.enumeration.StringHardCode;
 import logic.view.desktop.controller.item.ItemController;
+import logic.view.desktop.controller.item.ItemMapGController;
+import logic.view.desktop.controller.item.ItemReviewGController;
 import javafx.scene.image.Image;
 
 import java.util.List;
@@ -27,7 +36,7 @@ public class HuntInformationGController extends ItemController{
     private AnchorPane ancPane;
 
 	@FXML
-    private ListView<HuntBean> lvHuntReview;
+    private ListView<ReviewBean> lvHuntReview;
 
     @FXML
     private Label lbHuntName;
@@ -76,15 +85,37 @@ public class HuntInformationGController extends ItemController{
 		
 		var username = (String) itemList.get(1);
 		
-		var controller = new ManageHuntControl();
+		var controllerHunt = new ManageHuntControl();
+		var controllerPlay = new PlayHuntControl();
 		
-		var hb = controller.getHunt(idHunt, username);
+		var hb = controllerHunt.getHunt(idHunt, username);
 		lbHuntName.setText(hb.getHuntName());
 		lbCreatorName.setText(hb.getUsername());
 		var map = hb.getMap();
 		imgMap.setImage(new Image("File:" + map.getImage(), imgMap.getFitWidth(), imgMap.getFitHeight(), false, false));
 		rtHunt.setRating(hb.getAvgRating());
 		rtHunt.setDisable(true);
+		
+		ObservableList<ReviewBean> reviewsList = FXCollections.observableArrayList();
+		
+		reviewsList.setAll(controllerPlay.getReview(hb));
+		
+		lvHuntReview.setItems(reviewsList);
+		lvHuntReview.setCellFactory(review -> new ListCell<ReviewBean>() {
+			@Override
+			public void updateItem(ReviewBean itemBean, boolean empty) {
+				super.updateItem(itemBean, empty);
+				if(itemBean != null) {
+					var newItem = new ItemReviewGController(Pages.ITEM_REVIEW, getIstance());
+					newItem.start(StringHardCode.REVIEW.toString(),itemBean);
+					setGraphic(newItem.getBox());
+					
+				} else {
+					setGraphic(null);
+					setText(null);
+				}
+			}
+		});
 		
 	}
 
---
+++ src/logic/view/desktop/controller/item/ItemReviewGController.java
@@ -0,0 +1,58 @@
+package logic.view.desktop.controller.item;
+
+import org.controlsfx.control.Rating;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import logic.bean.ReviewBean;
+import logic.enumeration.Pages;
+import logic.view.desktop.controller.ControllerWithLogin;
+
+public class ItemReviewGController extends ItemController{
+	
+    @FXML
+    private AnchorPane ancPane;
+
+    @FXML
+    private ImageView imgUserProfile;
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private Label lbUsername;
+
+    @FXML
+    private Rating rtHunt;
+
+    @FXML
+    private Label lbReviewText;
+    
+    @FXML
+    private Label lbDate;
+    
+	public ItemReviewGController(Pages page, ControllerWithLogin mainController) {
+		super(page, mainController);
+	}
+
+	@Override
+	public Parent getBox() {
+		return ancPane;
+	}
+
+	@Override
+	public void start(String arg, Object param) {
+		var review = (ReviewBean) param;
+		
+		rtHunt.setRating(review.getVote());
+		lbUsername.setText(review.getUsername());
+		lbReviewText.setText(arg);
+		lbDate.setText(review.getReviewDate().toString());
+		
+	}
+
+}
